1814e42752fa222dd3cec527e653e59cba92d8df
Javohir 3306 <r.javohir3306@gmail.com.com>
2022-04-20 16:33:32 +0500

    swerijgkhn

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header bordered class="bg-transparent text-white" style="position: absolute" >
+      <q-toolbar>
+        <q-toolbar-title>
+          <div class="text-h3" style="margin-left:40px;">
+          <span>
+            street
+          </span>
+          </div>
+        </q-toolbar-title>
+        <div class="row no-wrap justify-center items-center content-center" style="width:85%; bacground-color:transparent; height:100px;">
+        <q-btn-dropdown unelevated label="Dropdown Button" style="position:relative">
+          <option value="345076">HIGH</option>
+          <option value="345076">HIGH</option>
+          <option value="345076">HIGH</option>
+          <option value="345076">HIGH</option>
+        </q-btn-dropdown>
+        <q-btn-dropdown unelevated label="Dropdown Button">
+          <option value="345076">HIGH</option>
+          <option value="345076">HIGH</option>
+          <option value="345076">HIGH</option>
+        </q-btn-dropdown>
+        <q-btn-dropdown unelevated label="Dropdown Button"></q-btn-dropdown>
+        <q-btn-dropdown unelevated label="Dropdown Button"></q-btn-dropdown>
+        <q-btn-dropdown unelevated label="Dropdown Button"></q-btn-dropdown>
+        <q-btn-dropdown unelevated label="Dropdown Button"></q-btn-dropdown>
+        <q-btn-dropdown unelevated label="Dropdown Button"></q-btn-dropdown>
+        </div>
+        <div>
+        <q-btn flat round color="white" icon="" />
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+
+<script>
+
+export default ({
+
+})
+</script>
+<style scoped>
+  .q-toolbar{
+    position: absolute;
+  }
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,304 @@
+<template>
+<q-page>
+  <!-- 1-rasm -->
+      <q-img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/Group-2.png" alt=""
+      style="position: absolute;"/>
+      <div>
+        <q-img src="">
+          <div class="absolute-full text-subtitle2 flex flex-center">
+            LIVE BRIGHT RIDE HARD
+          </div>
+          <div class="text-h3 fit row wrap justify-center items-center content-center" style="margin-top:50px">
+            LIVE BRIGHT <span style="margin-left:15px;color:yellow;" class="bg-black">RIDE HARD</span>
+          </div>
+          <div class="fit row wrap justify-center items-start content-center" style="margin-top:120px">
+        <q-btn style="background: goldenrod; color: white" label="Goldenrod"></q-btn>
+        </div>
+        </q-img>
+      </div>
+
+
+
+
+<!-- videolar -->
+      <div class="fit row no-wrap justify-around items-center content-start" style="margin-top:240px;">
+
+        <div class="text-h6" style=" width:51%;color:grey;  ">
+          ABOUT US
+          <div class="text-h3" style="color:black; margin-top:15px">
+            When Snowboard Is <span style="background-color:black;color:yellow">Your Life</span>
+          </div>
+          <div style="width:51%;height:300px;margin-top:15px">
+        <span>Incredible scenery, an adrenaline rush or simply being out in the mountains are a few of the reasons people enjoy snowboarding. It's a great activity to get involved in and is more affordable than you might think if you plan ahead and are clever about a few bits and bobs.</span>
+          </div>
+        </div>
+         <q-video
+        style="width:30%; height:250px;"
+        src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"
+        />
+      </div>
+
+
+<!-- obuvlar -->
+    <div class="" style="margin-left:65px;">
+      <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/Group.png" style="margin-left:20px"><span style="color:grey; margin-left:20px">Equipment</span>
+      <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/if_cap_hat_ski_winter_2664884.png" style="margin-left:20px"><span style="color:grey;margin-left:20px">Clothes</span>
+      <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/if_smartwatch_watch_fitness_pulse_sport_2664858-2.png" style="margin-left:20px"><span style="color:grey;margin-left:20px">Accessories</span>
+    </div>
+<!-- 2-rasm -->
+<div style="margin-top:200px;">
+    <q-img src="http://xtrm.foxthemes.me/wp-content/uploads/2018/09/Group-26.png?id=3390">
+      <div class=" column absolute-full text-subtitle2 flex flex-center" style="margin-bottom:180px">
+        <span style="margin-left:155px" class="text-h5">
+          Events
+        </span>
+        <span style="margin-left:570px;margin-top:30px; width:30%" class="text-h4">
+          The Legendary Tyler Durden’s <span style="color:black;" class="bg-warning">Workshop</span>
+        </span>
+        <span style="width:30%;margin-left:575px;margin-top:30px">
+          It doesn’t matter if you are a stay at home mom, or a corporate professional. You need to keep your appearance at it’s best all the time, and that can be a hard job. But what if you don’t always have the time to go to a Snowboard for a professional facial? I’ve got just the solution for you.
+        </span>
+        <div style="margin-top:40px; margin-left:310px">
+          <q-btn style="background: goldenrod; color: white;" label="Buy now" />
+          <q-btn outline style="color: goldenrod; margin-left:10px" label="Learn more" />
+        </div>
+      </div>
+    </q-img>
+</div>
+<!-- faktlar -->
+<div class="fit row inline no-wrap justify-around items-center content-center" style="margin-top:80px">
+  <div class="column" >
+  <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/if-009-snowboard-2860878-1.png" alt="" srcset="" style="width:80px; height:90px;margin-left:90px">
+  <span class="text-h5" style="margin-top:20px" >Professional Equipment</span>
+  <span style="margin-top:20px">The board, bindings and boots are the major <br> purchase of snowboaring Gear. They are <br> also the 3 absolutely essential pieces you <br> must have in order to snowboard. While all <br> of the other items may be "desireable" these <br> three things you must have in order to <br> board.</span>
+  </div>
+
+  <div class="column">
+  <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/if-002-motocross-mask-2860885-1.png" alt="" srcset="" style="width:80px; height:90px;margin-left:90px">
+  <span class="text-h5" style="margin-top:20px">High Quality Accessories</span>
+  <span style="margin-top:20px">Snowboard goggles help battle glare and <br> protect your eyes from the snow and wind <br> while riding. When riding down a mountain <br> , snow and debris can fly against your face, <br> making goggles a necessary piece of <br> equipment. Insulated and waterproof <br> snowboard gloves are also very important.</span>
+  </div>
+
+  <div class="column">
+  <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/if-028-ski-jacket-2860859-1.png" alt="" srcset="" style="width:80px; height:90px;margin-left:90px">
+  <span class="text-h5" style="margin-top:20px">Comfortable Clothes</span>
+  <span style="margin-top:20px">A good snowboard jacket will have a wind <br> and waterproof outer shell. The jacket <br> should also be breathable, which means it <br> will allow sweat to escape and keep you <br> warm. lso, your socks should be high <br> enough so your skin does not rub against <br> your boots and cause irritation.</span>
+  </div>
+</div>
+<!-- 3-rasm -->
+<div>
+    <q-img style="margin-top:50px" src="http://xtrm.foxthemes.me/wp-content/uploads/2018/09/Group-38.png?id=3393">
+      <div class="column absolute-full text-subtitle2 flex flex-center">
+          <span style="margin-bottom:10px" class="text-h6">TESTIMONIALS</span>
+          <span class="text-h3" style="margin-bottom:50px">Our Riders Said About Us</span>
+          <!-- icons -->
+        <div class="row">
+          <q-icon name="star" color="yellow"></q-icon>
+          <q-icon name="star" color="yellow"></q-icon>
+          <q-icon name="star" color="yellow"></q-icon>
+          <q-icon name="star" color="yellow"></q-icon>
+          <q-icon name="star" color="yellow"></q-icon>
+        </div>
+        <div class="column align-center" style="margin-top:20px">
+          <span class=" text-h5">“Everything was as expected and agreed. They know their thing and also</span>
+          <span class="text-h5" style="margin-left:240px">know the place very well.”</span>
+        </div>
+        <div class="row" style="margin-top:30px">
+          <q-avatar>
+            <img style="width: 400;height: 400;" src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/Group-13.png">
+          </q-avatar>
+          <div class="column">
+          <span class="text-h5" style="margin-left:20px;">Andrew Gosho</span>
+          <span style="margin-left:20px">Pro Rider</span>
+          </div>
+        </div>
+      </div>
+    </q-img>
+</div>
+<div style="">
+<!-- 1-Shopping -->
+  <div style="margin-top:150px">
+    <span style="" class="text-h4"> Let’s Go</span><span class="text-h4" style="color:yellow;background-color:black;">Shopping</span>
+  </div>
+<div class="row justify-around content-center">
+<div style="width:200px;height:400px;margin-left:50px;margin-top:50px" class="column no-wrap">
+      <q-card class="my-card">
+
+        <span style="color:black;background-color:yellow;" class="text-h6">Sn boots</span>
+        <span style="background-color:black;color:white; margin-left:60px" class="text-h5">Sale</span>
+      <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/group-30-1.png">
+    </q-card>
+    <span style="color:rgb(31, 44, 44);margin-top:20px;" class="text-h6" >Snowboard boots</span>
+    <span class="row">
+<s>E359.00</s><span style="margin-left:10px;">E359.00</span>    </span>
+</div>
+<div style="width:200px;height:400px;margin-left:50px;margin-top:50px" class="column no-wrap">
+      <q-card class="my-card">
+
+        <span style="color:black;background-color:yellow;" class="text-h6">Sn boots</span>
+        <span style="background-color:black;color:white; margin-left:60px" class="text-h5">Sale</span>
+      <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/group-33-1.png" style="margin-bottom:50px">
+    </q-card>
+    <span style="color:rgb(31, 44, 44);margin-top:20px;" class="text-h6" >Snowboard boots</span>
+    <span class="row">
+<s>E359.00</s><span style="margin-left:10px;">E359.00</span>    </span>
+</div>
+<div style="width:200px;height:400px;margin-left:50px;margin-top:50px" class="column no-wrap">
+      <q-card class="my-card">
+
+        <span style="color:black;background-color:yellow;" class="text-h6">Sn boots</span>
+        <span style="background-color:black;color:white; margin-left:60px" class="text-h5">Sale</span>
+      <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/group-36-1.png">
+    </q-card>
+    <span style="color:rgb(31, 44, 44);margin-top:20px;" class="text-h6" >Snowboard boots</span>
+    <span class="row">
+<s>E359.00</s><span style="margin-left:10px;">E359.00</span>    </span>
+</div>
+<div style="width:200px;height:1000px;margin-left:50px;margin-top:50px" class="column no-wrap">
+      <q-card class="my-card">
+
+        <span style="color:black;background-color:yellow;" class="text-h6">Sn boots</span>
+        <span style="background-color:black;color:white; margin-left:60px" class="text-h5">Sale</span>
+      <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/group-32-1.png" style="margin-bottom:170px">
+    </q-card>
+    <span style="color:rgb(31, 44, 44);margin-top:20px;" class="text-h6" >Snowboard boots</span>
+    <span class="row">
+<s>E359.00</s><span style="margin-left:10px;">E359.00</span>    </span>
+</div>
+<div style="width:200px;height:400px;margin-left:50px;margin-top:50px" class="column no-wrap">
+      <q-card class="my-card">
+
+        <span style="color:black;background-color:yellow;" class="text-h6">Sn boots</span>
+        <span style="background-color:black;color:white; margin-left:60px" class="text-h5">Sale</span>
+      <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/group-29-1.png" style="margin-bottom:30px">
+    </q-card>
+    <span style="color:rgb(31, 44, 44);margin-top:20px;" class="text-h6" >Snowboard boots</span>
+    <span class="row">
+      <s>E359.00</s><span style="margin-left:10px;">E359.00</span>
+    </span>
+<!-- 2-shopping -->
+</div>
+  <div class="row justify-around content-center" style="width:100%; height:0.5px">
+  <div style="width:200px;height:400px;margin-left:50px;margin-top:50px" class="column no-wrap">
+        <q-card class="my-card">
+
+          <span style="color:black;background-color:yellow;" class="text-h6">Sn boots</span>
+          <span style="background-color:black;color:white; margin-left:60px" class="text-h5">Sale</span>
+        <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/group-28-1.png" style="height:250px;width:125px">
+      </q-card>
+      <span style="color:rgb(31, 44, 44);margin-top:20px;" class="text-h6" >Snowboard boots</span>
+      <span>
+      <s>E359.00</s><span style="margin-left:10px;">E359.00</span>
+      </span>
+  </div>
+  <div style="width:200px;height:400px;margin-left:50px;margin-top:50px" class="column no-wrap">
+        <q-card class="my-card">
+
+          <span style="color:black;background-color:yellow;" class="text-h6">Sn boots</span>
+          <span style="background-color:black;color:white; margin-left:60px" class="text-h5">Sale</span>
+        <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/group-35-1.png" style="margin-bottom:50px">
+      </q-card>
+      <span style="color:rgb(31, 44, 44);margin-top:20px;" class="text-h6" >Snowboard boots</span>
+      <span>
+      <s>E359.00</s><span style="margin-left:10px;">E359.00</span>
+      </span>
+  </div>
+  <div style="width:200px;height:400px;margin-left:50px;margin-top:50px" class="column no-wrap">
+        <q-card class="my-card">
+
+          <span style="color:black;background-color:yellow;" class="text-h6">Sn boots</span>
+          <span style="background-color:black;color:white; margin-left:60px" class="text-h5">Sale</span>
+        <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/group-37-1.png" style="height:250px;width:125px">
+      </q-card>
+      <span style="color:rgb(31, 44, 44);margin-top:20px;" class="text-h6" >Snowboard boots</span>
+      <span>
+      <s>E359.00</s><span style="margin-left:10px;">E359.00</span>
+      </span>
+  </div>
+  <div style="width:200px;height:1000px;margin-left:50px;margin-top:50px" class="column no-wrap">
+        <q-card class="my-card">
+
+          <span style="color:black;background-color:yellow;" class="text-h6">Sn boots</span>
+          <span style="background-color:black;color:white; margin-left:60px" class="text-h5">Sale</span>
+        <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/group-31-1.png" style="width:125px;height:250px">
+      </q-card>
+      <span style="color:rgb(31, 44, 44);margin-top:20px;" class="text-h6" >Snowboard boots</span>
+      <span>
+      <s>E359.00</s><span style="margin-left:10px;">E359.00</span>
+      </span>
+  </div>
+  <div style="width:200px;height:400px;margin-left:50px;margin-top:50px" class="column no-wrap">
+        <q-card class="my-card">
+
+          <span style="color:black;background-color:yellow;" class="text-h6">Sn boots</span>
+          <span style="background-color:black;color:white; margin-left:60px" class="text-h5">Sale</span>
+        <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/group-34-1.png" style="margin-bottom:120px">
+      </q-card>
+      <span style="color:rgb(31, 44, 44);margin-top:20px;" class="text-h6" >Snowboard boots</span>
+      <span>
+      <s>E359.00</s><span style="margin-left:10px;">E359.00</span>
+      </span>
+  </div>
+  </div>
+  </div>
+</div>
+
+
+
+<div>
+  <q-img src="http://xtrm.foxthemes.me/wp-content/uploads/2018/09/Group-27.png?id=3467">
+    <div class="absolute-full text-subtitle2 flex flex-center">
+      <div class="fit row no-wrap justify-around items-center content-center" style="margin-right:11%">
+      <span class="column">
+        <span class="text-h3" style="margin-left:75px"> 127</span><span class="text-h6">Miles of Awesome Trails</span>
+      </span>
+      <span class="column">
+        <span class="text-h3" style="margin-left:80px">38</span><span class="text-h6">Professional Instructors</span>
+      </span>
+      <span class="column">
+        <span class="text-h3" style="margin-left:50px">3,500</span><span class="text-h6">Feets of Height Difference</span>
+      </span>
+      <span class="column">
+        <span class="text-h3" style="margin-left:20px">120 k</span><span class="text-h6">Riders Every Year</span>
+      </span>
+      </div>
+    </div>
+  </q-img>
+</div>
+
+<div class="column" style="margin-left:200px;margin-top:90px">
+  <span>PRICING</span>
+  <span class="text-h4" style="margin-top:30px">Why Pay <span style="background-color:black;color:yellow"> More?</span></span>
+</div>
+
+<div style="width: 20%;">
+  <q-card dark bordered class="bg-grey-9 my-card">
+    <q-card-section>
+      <div class="text-h6">Our Changing Planet</div>
+      <div class="text-subtitle2">by John Doe</div>
+    </q-card-section>
+
+    <q-separator dark inset />
+
+    <q-card-section style="height: 300px;">
+
+    </q-card-section>
+  </q-card>
+</div>
+
+</q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
+<style scoped>
+.q-img__content > div{
+  background-color: transparent;
+}
+
+</style>
